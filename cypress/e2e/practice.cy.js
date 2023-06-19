fdec59bd47d76d3556b2277c3368aca2265efc7d
kishorS18 <kishor.sharma@arq.group>
2023-06-20 09:34:07 +1000

    upload file

--- cypress/e2e/practice.cy.js
+++ cypress/e2e/practice.cy.js
@@ -1,14 +1,20 @@
-describe('Practice Cypress Test', () => {
-  it('positive test', () => {
-    cy.visit("https://demoqa.com/")
-    cy.get("h5").contains("Forms").click()
-    cy.contains("Practice Form").click()
+describe("Practice Cypress Test", () => {
+  it("positive test", () => {
+    cy.visit("https://demoqa.com/");
+    cy.get("h5").contains("Forms").click();
+    cy.contains("Practice Form").click();
     //Filling form
-    cy.get("#firstName").type("Kysor")
-    cy.get("#lastName").type("Srma")
-    cy.get('#userEmail').type("test@email.com")
-    cy.get("#gender-radio-1").check({ force: true})
-    cy.get("#userNumber").type("123456789")
+    cy.get("#firstName").type("Kysor");
+    cy.get("#lastName").type("Srma");
+    cy.get("#userEmail").type("test@email.com");
+    cy.get("#gender-radio-1").check({ force: true });
+    cy.get("#userNumber").type("123456789");
+    cy.get("#subjectsContainer").type("testSubject");
+    cy.get("#hobbies-checkbox-1").check({ force: true });
+    // upload file
+    const filePath = './testImage.png';
+    cy.get("#uploadPicture").attachFile(filePath);
+    cy.get("#currentAddress").type("test Address");
 
-  })
-})
+  });
+});
