9a5f3bd462888bc84814713fb411a2bf917afab4
kishorS18 <kishor.sharma@arq.group>
2023-06-21 10:23:06 +1000

    iframe positive test

--- cypress/e2e/iFrame.cy.js
+++ cypress/e2e/iFrame.cy.js
@@ -3,7 +3,7 @@ describe("iFrame", () => {
     cy.visit("https://demoqa.com/");
     cy.get("h5").contains("Alerts, Frame & Windows").click();
     cy.get(".text").contains("Frames").click();
-    cy.iframe("#frame1").find("#sampleHeading").should("have.text", "This is a sample page1")
+    cy.iframe("#frame1").find("#sampleHeading").should("have.text", "This is a sample page")
     
     
   });
