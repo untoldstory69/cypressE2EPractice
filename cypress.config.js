3812a79a6a9b69affaea913f70812f71194f0c51
kishorS18 <kishor.sharma@arq.group>
2023-08-25 13:05:27 +1000

    mochawesome report added

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'g4f73q',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       //baseUrl: "https://demoqa.com/automation-practice-form"
-      chromeWebSecurity: false
+      chromeWebSecurity: false;
     },
   },
-  chromeWebSecurity: false
+
+  reporter: "../node_modules/mochawesome/src/mochawesome.js",
+  reporterOptions: {
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+  chromeWebSecurity: false,
 });
